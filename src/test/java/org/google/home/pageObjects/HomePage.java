e5d0ae6b7376c7648fc7f09199bd25f110b7f82a
SuriyaAutomation <154884166+SuriyaAutomation@users.noreply.github.com>
2025-03-26 12:00:28 +0530

    Update HomePage.java

--- src/test/java/org/google/home/pageObjects/HomePage.java
+++ src/test/java/org/google/home/pageObjects/HomePage.java
@@ -11,9 +11,26 @@ public class HomePage {
 
 	By txtHomePageValidation = By.xpath("//div[text()='Google']");	
 
+	//dynamic elements 
+	private static final By tblDonation = By.xpath("//main[@id='main-container']//table/tbody");
+	private static final String txtAssUserId = "//input[@id='ldap_input%s']";
+	private static final String popUpRecurringDonation = "//p[text()='%s' and contains(text(),'donations') ]";
 		
 	public HomePage(WebDriver driver) {
 		this.driver = driver;
+	}
+	
+	public static String prepareXPathString(String xpath, String value) {
+		return String.format(xpath, value);
+	}
+
+	public String getpopUpRecurringDonationMsg() {
+		String txtRecurringDonation = SeleniumHelper.getTextFromElement(driver, By.xpath(prepareXPathString(popUpRecurringDonation,String.valueOf("WelcomeHome"))));
+		return txtRecurringDonation;
+	}
+	
+	public void clickpopUpRecurringDonationMsg() {
+		SeleniumHelper.clickOnElement(driver, By.xpath(prepareXPathString(popUpRecurringDonation,String.valueOf("WelcomeHome"))));		
 	}
 	
 	public String getHomePageTitle() {
